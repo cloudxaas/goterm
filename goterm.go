d8f39d911840695a9c36d6f2bf84dc5910655e77
ultperf <104323920+ultperf@users.noreply.github.com>
2023-03-20 11:10:21 -0700

    Create goterm.go

---
+++ goterm.go
@@ -0,0 +1,30 @@
+package goterm
+
+import (
+        "fmt"
+        "strings"
+)
+
+
+//if yn == 1, Yes as default otherwise No as default
+func YesNo(uint8 yn) uint8 {
+        if yn == 0 {
+                fmt.Printf("(y/N): ")
+        }else{  
+                fmt.Printf("(Y/n): ")
+        }
+
+        var s string
+
+        for {   
+                _, err := fmt.Scan(&s)
+                if err == nil {
+                        break
+                        //fmt.Print(err)
+                }
+        }
+        if strings.ToLower(strings.TrimSpace(s)) == "y" {
+                return 1
+        }
+        return 0
+}
