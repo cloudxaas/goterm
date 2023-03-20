9f8ba1497c2e8dec9bfc2b684aff2810f4274422
ultperf <104323920+ultperf@users.noreply.github.com>
2023-03-20 11:27:17 -0700

    Update goterm.go

--- goterm.go
+++ goterm.go
@@ -1,37 +1,35 @@
 package goterm
 
 import (
-        "fmt"
-        "strings"
+	"fmt"
+	"log"
+	"strings"
 )
 
-
-//if yn == 1, Yes as default otherwise No as default
+// if yn == 1, Yes as default otherwise No as default
 func YesNo(yn uint8) uint8 {
-        if yn == 0 {
-                fmt.Printf("(y/N): ")
-        }else{  
-                fmt.Printf("(Y/n): ")
-        }
+	if yn == 0 {
+		fmt.Printf("(y/N): ")
+	} else {
+		fmt.Printf("(Y/n): ")
+	}
 
-        
-        var s string
-        var err error
-        for {   
-                
-                s, err = reader.ReadString('\n')
+	var s string
+	var err error
+	for {
+
+		s, err = reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-                _, err := fmt.Scan(&s)
-                if err == nil {
-        if strings.ToLower(strings.TrimSpace(s)) == "y" {
-                return 1
-        }
-                       
-                        
-                }
+		_, err := fmt.Scan(&s)
+		if err == nil {
+			if strings.ToLower(strings.TrimSpace(s)) == "y" {
+				return 1
+			}
 
-        }
-        return 0
+		}
+
+	}
+	return 0
 }
